c8af63b89586fc69f4387d3224f8b6707daa2f96
Marcos Vinicius <marcosvinicius.mota@gmail.com>
2022-09-28 19:39:05 -0300

    comentario só presta no final

--- elementCreator.js
+++ elementCreator.js
@@ -1,4 +1,5 @@
 
+//Quase que um construtor de classe. Cria uma variável composta e retorna
 function createProduct(_name, _category, _price, image_src) {
     var product = {
         name : _name,
@@ -9,63 +10,86 @@ function createProduct(_name, _category, _price, image_src) {
     return product;
 }
 
-
+//Aqui ocorre a atualização do html
 function appendElement(element_id, product, is_visible){
+
+    //aqui ocorre a seleção da seção
     var section = document.getElementById(element_id);
 
+    //Criação de uma div para englobar tudo
     product_div   = __createElement("div");
+    //criação de uma tag de iamgem
     product_image = __createElement("img");
+    //criação do <p> do nome do produto
     product_name  = __createElement("p");
+    //criação do <p> do preço do produto
     product_price = __createElement("p");
+    //ver detalhes
     see_more      = __createElement("a");
 
-    __appendElements(section, product_div);
+    //A div passa a ser filha da section
+    __appendChild(section, product_div);
 
+    //adição da classe product_image para as imagens. Lá tem as dimensões da imagem
     product_image.classList.add("product_image");
+    //jogando o atributo src na imagem
     product_image.setAttribute("src", "images/" + product.img + ".png");
 
     __updateContentText(see_more, "Ver produto");
     see_more.setAttribute("href", "index.html");
 
+    //tag do nome recebe o texto com o nome do produto
     __updateContentText(product_name, product.name);
+    //tag do preço recebe o preço com o padrão de moeda americano
     __updateContentText(product_price, product.price.toLocaleString('en',{style: 'currency', currency: 'USD'}));
     
+    //os ultimos 2 elementos terão essa classe para eles sumirem quando a tela for pequena
     if(!is_visible){
         product_div.classList.add("invisible");
     }
 
+    //Jogando classes nos elementos
     product_div.classList.add("product_div");
     product_name.classList.add("product_name");
     product_price.classList.add("product_price");
 
-    __appendElements(product_div, product_image);
-    __appendElements(product_div, product_name);
-    __appendElements(product_div, product_price);
-    __appendElements(product_div, see_more);
+    //Todas as tags entram em div
+    __appendChild(product_div, product_image);
+    __appendChild(product_div, product_name);
+    __appendChild(product_div, product_price);
+    __appendChild(product_div, see_more);
 
 }
-
+//função para chamar appendElement, só que em um array
 function appendElementArray(element_id, products, size, visibilityCondition){
     for(var i = 0; i < size; i++){
         appendElement(element_id, products[i], i < visibilityCondition);
     }
 }
 
+//Função para iniciar os elementos de forma genérica
+//recebe o array de produtos vazio, o tamanho dele, e os elementos do produtos
 function startElementsGeneric(products, size, name, category, price, image_src){
-    const temp = "123456"
+    const temp = "123456";
     for(var i = 0; i < size; i++){
+        //cria o produto e joga no array
         products[i] = createProduct(name, category, price ,image_src);
-        image_src = image_src.replace(image_src.charAt(image_src.length - 1).toString(), temp.charAt(i))
+
+        //O elemento image_src tem apenas o ultimo caractere diferente em cada uma das seções
+        //O objetivos dessa linha é pegar o número que está no final do nome da imagem e atribuir uma unidade a mais, aí na próxima criação de produto, será outra iamgem
+        //Ao abrir a pasta de imagens, ficará tudo mais claro
+        image_src = image_src.replace(image_src.charAt(image_src.length - 1), temp.charAt(i));
     }
 }
 
 function main(){
 
+    //vetores com cada umas das seções
     var firstSec = [];
     var secondSec = [];
     var thirdSec = [];
 
-
+    //joga elementos padrões em casa um das seções
     startElementsGeneric(firstSec, 6, "ProdutoXYZ", "Star Wars", 60.0, "skill0");
     __updateContentText(document.getElementById("title1"), "Star Wars");
 
@@ -75,6 +99,7 @@ function main(){
     startElementsGeneric(thirdSec, 6, "ProdutoXYZ", "Diversos", 60.0, "diversos0");
     __updateContentText(document.getElementById("title3"), "Diversos");
 
+    //ordem : id , array, tamanho, a partir do qual elemento ele passa a ser invisivel
     appendElementArray("first_section", firstSec, 6, 4);
     appendElementArray("second_section", secondSec, 6, 4);
     appendElementArray("third_section", thirdSec, 6, 4);
--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 
-function __appendElements(main_element, child){
+//Algumas funções para transformar métodos de objeto em estáticos <Fica um pouco mais bonito kkkk>
+function __appendChild(main_element, child){
     main_element.appendChild(child);
 }
 
@@ -15,28 +16,57 @@ function __getElementPositionY(element){
     return element.getBoundingClientRect();
 }
 
+//Função para dar scroll até um certo ponto da página. Ela tende a deixar o elemento na posição 25% da tela.
 function __viewCategory(element){
     var coord = element.getBoundingClientRect().top;
     scroll(0, coord - (coord * 0.25));
 }
 
+//Função para comparar um certo numero de caracteres em duas strings, a partir de um certo índice
+function compareString(string1, string2, startFrom, numberOfCharacters){
+    var j = 0;
+    //sempre começando do indice escolhido até o tamanho da segunda string
+    //Essa soma serve pra quando o índice inicial for maior que a string2.
+    //Imagine a seguite sitação; var i = 5; i < 4; i++, isso pode ocorrer sem a soma
+    //Com a soma, seria; var i = 5; i < 5 + 4; i++;
+    for(var i = startFrom; i < numberOfCharacters + startFrom; i++){
+        if(string1[i] != string2[j++])
+            return false;
+    }
+    return true;
+}
+
+//Retorna de a string2 está contida na string1
+function subString(string1, string2){
+    
+    //Esse for vai controlar qual posição da string vai ser comparada na função compareString
+    for(var i = 0; i < string1.length - string2.length + 1; i++){
+        //A comparação sempre vai ter como base o tamanho da segunda string
+        if(compareString(string1, string2, i, string2.length))
+            return true;
+    }
+    return false;
+}
+
+//Função apenas para dar uma interação "divertida" na página
 function youCantSearchForItens(){
 
-    const searchContent = document.getElementById("search_bar").value;
-
+    //Conteúdo da caixa de texto
+    const searchContent = document.getElementById("search_bar").value.toLowerCase();
+    
     if(searchContent.length == 0){
         alert("Campo de pesquisa vazio");
         return;
     }
     var section = "";
 
-    if(searchContent == "star wars"){
+    if(subString("star wars", searchContent)){
         section = "first_section";
     }
-    else if(searchContent == "consoles"){
+    else if(subString("consoles", searchContent)){
         section = "second_section";        
     }
-    else if(searchContent == "diversos"){
+    else if(subString("diversos", searchContent)){
         section = "third_section";
     }
     else{
@@ -44,11 +74,11 @@ function youCantSearchForItens(){
         return;
     }
 
+    //Função de dar scroll sendo chama no elemento que tem o ID da variavel section
     __viewCategory(document.getElementById(section));
-
-    
 }
 
+//pequena interação para dar aviso ao enviar mensagem
 function sendMessage(){
     const name = document.querySelector("#name").value;
     const message = document.querySelector("#message").value;
@@ -59,20 +89,27 @@ function sendMessage(){
         alert("Messagem enviada com sucesso");
 }
 
+//Ao clicar em ver mais, vc irá se deparar com a triste situação de não ter mais produtos
 function youCantSeeMore(){
     alert("Não há mais produtos")
 }
 
+//Função para forçar que os campos de login sejam preenchidos
 function confirmLogin(){
 
     const user = document.querySelector("#user").value;
     const password = document.querySelector("#password").value;
 
+    //Se algum dos campos estiverem vazios, será criado um <p> para dar uma regulagem ao usuário informando que os campos devem ser preenchidos
     if(user.length == 0 || password.length == 0){
+
         var loginDIV = document.getElementById("login_div");
         
+        //Aqui ocorre uma tentativa de pegar o parágrafo do aviso
         p = document.getElementById("nulltest");
 
+        //Caso seja null, ele cria o elemento, atribrui seus estilos e seu id
+        //Se tirar esse teste, ele cria toda vez que clicar no botão, ao invés de só uma vez
         if(p == null){
             var p = __createElement("p");
             __updateContentText(p, "Preencha os campos com valores válidos");
@@ -81,9 +118,11 @@ function confirmLogin(){
             p.style.color = "red";
             p.style.fontWeight = "bold";
 
-            __appendElements(loginDIV, p);  
+            // p passa a ser filho de loginDIV
+            __appendChild(loginDIV, p);  
         }
     }
+    //Caso esteja tudo correto, vc vai pra pagina de login
     else{
         window.location.href = "index.html";
     }
